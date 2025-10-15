35dcdfc0595c185fa8246c2899b4d1f1249d351a
amcook <amcook127@gmail.com>
2025-10-15 13:10:37 -0300

    updates tocpue

--- Fishery/CPUE/4.spatial33_34_41CPUE_sdmTMB.r
+++ Fishery/CPUE/4.spatial33_34_41CPUE_sdmTMB.r
@@ -179,6 +179,20 @@ cas$Y = st_coordinates(cas)[,2]
 ca = as_tibble(cas)
 
 saveRDS(list(gto,ns_coast,ca),file='data_33_34_41_sdmtmb.rds')
+require(mgcv)
+require(bio.lobster)
+require(spdep)
+require(sf)
+require(devtools)
+require(dplyr)
+require(sdmTMB)
+require(purrr)
+la()
+
+setwd(file.path(project.datadirectory('Framework_LFA33_34_41')))
+
+
+
 m = readRDS(file='data_33_34_41_sdmtmb.rds')
 gto = m[[1]]
 ns_coast = m[[2]]
@@ -197,9 +211,8 @@ bspde <- sdmTMBextra::add_barrier_mesh(
   mes, ns_coast,range_fraction = .2,
   proj_scaling = 1, plot = TRUE
 )
-
-m4 = sdmTMB(WEIGHT_KG~ 0+
-                       s(GlorBC_mean)+s(DOS),
+require(sdmTMB)
+m4 = sdmTMB(WEIGHT_KG~ s(GlorBC_mean)+s(DOS),
             offset = 'leffort',
             data=ca,
             family = nbinom2(link='log') ,
@@ -210,8 +223,8 @@ m4 = sdmTMB(WEIGHT_KG~ 0+
             time='SYEAR',
            spatiotemporal='IID'
 )
- b = visreg::visreg(m4,xvar='DOS',scale='response')
- g = visreg::visreg(m4,xvar='GlorBC_mean',scale='response')
+# b = visreg::visreg(m4,xvar='DOS',scale='response')
+# g = visreg::visreg(m4,xvar='GlorBC_mean',scale='response')
 
  ##predictions
  gtos = subset(gto, LFA %ni% 41, select=c(LFA,GRID_NO,X,Y))
@@ -221,7 +234,33 @@ m4 = sdmTMB(WEIGHT_KG~ 0+
  
  t1 = expand.grid(DOS=dos,GlorBC_mean=temps,SYEAR=yr)
  pre = merge(gtos,t1)
- 
- 
+ require(purrr)
+base_subsets <- map(1:800, function(i) {
+			pre %>%
+			group_by(SYEAR) %>%
+			slice_sample(n = 1, replace = TRUE) %>%
+			ungroup()
+																							                      })
+sampled_ids <- bind_rows(base_subsets) %>% distinct()
+remaining_df <- anti_join(pre, sampled_ids)
+
+# Step 3: Randomly distribute remaining rows across the 200 subsets
+remaining_split <- split(remaining_df, rep(1:800, length.out = nrow(remaining_df)))
+
+# Step 4: Combine base samples with remaining rows
+final_subsets <- map2(base_subsets, remaining_split, bind_rows)
+
+years = unique(ca$SYEAR)
+for(i in 1:length(final_subsets)) {
+	        fs = final_subsets[[i]]
+        fs = subset(fs, SYEAR %in% years)
+	g = predict(m4,newdata=fs,se_fit=F)
+	        fs$pred = m4$family$linkinv(g$est)
+		        final_subsets[[i]] = fs
+		        saveRDS(fs, file=paste0('cpue_predictions',i,'.rds'))
+			        rm(fs,g)
+}
+
+ mod = predict(m4, newdata=pre, return_tmb_object=T)
  
  
